c3b7148f57dd4813c567abbc08dc03b60d45de23
Chun Yang <yang2007chun@gmail.com>
2015-10-30 14:25:21 +0800

    export all scss files

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,28 @@ gulp.task('js', function(){
 });
 
 gulp.task('css', function(){
-  return gulp.src(['./mfb/src/mfb.css', './mfb/src/mfb.css.map', './mfb/src/mfb.scss'])
+  return gulp.src([
+    './mfb/src/mfb.css',
+    './mfb/src/mfb.css.map',
+  ])
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('default', ['js', 'css']);
+gulp.task('scss-main', function () {
+  return gulp.src([
+    './mfb/src/mfb.scss',
+  ])
+    .pipe(gulp.dest('./'))
+});
+
+gulp.task('scss-modules', function () {
+  return gulp.src([
+    './mfb/src/_/_fountain.scss',
+    './mfb/src/_/_slidein-spring.scss',
+    './mfb/src/_/_slidein.scss',
+    './mfb/src/_/_slidein.scss',
+  ])
+    .pipe(gulp.dest('./_/'))
+});
+
+gulp.task('default', ['js', 'css', 'scss-main', 'scss-modules']);
