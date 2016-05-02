2a990bf31bf243a6297b21685278f282e60baabe
brianscroggins24 <brian.scroggins24@gmail.com>
2016-05-02 10:46:26 -0500

    updated react to version 15.0.0

--- components/mainButton.jsx
+++ components/mainButton.jsx
@@ -1,7 +1,7 @@
 import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
 
 checkNpmVersions({
-  'react': '0.14.8'
+  'react': '15.0.0'
 }, 'poetic:meteor-react-material-floating-button');
 
 const React = require('react');
